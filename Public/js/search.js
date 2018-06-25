510ccc33023c37ffd379ba027608aab22f3581ef
Gail Matsushima <gail.matsushima@gmail.com>
2018-06-24 23:53:57 -1000

    search via tab list is working, will start working on search to go through list AND content then styling of results located

--- Public/js/search.js
+++ Public/js/search.js
@@ -6,10 +6,15 @@ function myFunction() {
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
+        console.log(li[i]);
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            //console.log(a.innerHTML);
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         }
     }
 }
+/**LEFT OFF: start working on a search via the tab content, in addition
+tab list, then working on styling for results (list and content) located
+**/
