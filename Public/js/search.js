6ba23602224caa0eabd9bd4121ab68fd88196d69
Gail Matsushima <gail.matsushima@gmail.com>
2018-07-14 00:49:03 -0700

    search is working with innerHTML just need to find a way to include tabs and tab pane in for loop

--- Public/js/search.js
+++ Public/js/search.js
@@ -2,20 +2,32 @@ function myFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    container = document.getElementsByClassName("container");
-    textReview = container[0].textContent.toUpperCase().search(filter);
+    tabs = document.getElementById("myUL").innerHTML;
+    tabContent = document.getElementsByClassName("tab-pane");
 
-    if (textReview > 0) {
-        console.log('I found ' + filter + ' at ' + textReview);
-    } else {
-        console.log('No results matched your search. Please try a different search');
+    for(i=0; i < tabContent.length; i++) {
+      console.log('tabContent: ', tabContent[i].innerHTML);
+      var textReview = tabContent[i].innerHTML.toUpperCase().search(filter);
+      if(textReview > 0){
+        console.log('found result here: ' + textReview);
+      }
+      else{
+        console.log('no results');
+      }
     }
+
+
+
+    /**if (textReview > 0 && event.keyCode === 13) {
+        $(textLocator).css("border", "solid red 2px");
+    } else {
+        //console.log('No results matched your search. Please try a different search');
+    }**/
 }
 /**
 TO DO'S:
-1. Search is working for tabs AND tabs' content
+1. Search is working for tabs' content
+1.b. Need to also find a way to also search through 'tabs' and
 2. Once phrase or letters inputted, enter will return results
 3. Style Results
 4. Start working on accordions
